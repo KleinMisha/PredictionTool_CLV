7d068d766d87f84a58c61782365044ddd9369219
Misha Klein <klein.misha@gmail.com>
2018-03-08 17:38:23 +0100

    forgot to generate the file with the experimental values.

--- code/subset_data_from_library.py
+++ code/subset_data_from_library.py
@@ -22,10 +22,14 @@ PAM_seq = 'NGG'
 MM_count = [1,2]
 
 
-filename = "Pattanayak_" + guide_name + "_" + concentration + "_" + PAM_seq
+file_seq = "Pattanayak_" + guide_name + "_" + concentration + "_" + PAM_seq
 for mm_count in MM_count:
-    filename += '_'+str(mm_count) + 'MM'
-filename += "_sequences.txt"
+    file_seq += '_'+str(mm_count) + 'MM'
+
+
+file_score = file_seq
+file_seq += "_sequences.txt"
+file_score += "_scores.txt"
 
 
 Pattanayak = pd.read_excel('../data/Pattanayak_Liu_NBT_2673_Data.xlsx')
@@ -38,4 +42,8 @@ for i in range(len(training)):
     row = training.iloc[i]
     training_sequences.append(get_homology(row)[:-2])
 
-np.savetxt(filename, training_sequences)
+
+training_scores = training['Score']
+
+np.savetxt(file_seq, training_sequences)
+np.savetxt(file_score, training_scores)
